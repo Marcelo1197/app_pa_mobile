4c8ac5faeded19b75b51d949f5b60f12a017022a
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2021-04-19 01:24:40 -0300

    OK: Transformamos hashtags en <a> elements

--- src/components/Home/charla.js
+++ src/components/Home/charla.js
@@ -5,8 +5,10 @@ const html = htm.bind(createElement);
 
 import TarjetaTexto from "./tarjetaTexto.js";
 
-async function leerTextos(idCharla) { //A: trae todos los textos de una charla
-  const res = await fetchConToken( //A: funcion definida en auth-servicio para acceder a la API con un token
+async function leerTextos(idCharla) {
+  //A: trae todos los textos de una charla
+  const res = await fetchConToken(
+    //A: funcion definida en auth-servicio para acceder a la API con un token
     `https://si.podemosaprender.org/api/charla/${idCharla}`
   );
   return res;
@@ -15,25 +17,51 @@ async function leerTextos(idCharla) { //A: trae todos los textos de una charla
 class Charla extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { textos: [] };
+    this.state = { listaTextos: [] };
   }
 
-  componentDidMount() { //A: cuando componente montado trae textos y los setea al state
-    const res = leerTextos(this.props.idCharla)
-      .then((txt) => txt.json())
-      .then((txt) => this.setState({ textos: txt.textos }));
-    //DBG:console.log(this.props.charlas);
+  hashtagsALinks(listaTextos) {
+    let textosCharla = listaTextos;
+    //A: Recorro cada texto de la charla renderizada
+    //Y reemplazo cada '#tag' por el mismo '#tag' entre etiquetas <a></a> para poder clickearlos
+    textosCharla.forEach((textoItem) => {
+      textoItem.texto = textoItem.texto.replace(
+        /#([A-Za-z0-9_]+)/g,
+        this.creaLink
+      );
+    });
+    //DBG:console.info(textosCharla);
+    return textosCharla;
+  }
+
+  creaLink(hashtag) {
+    //A: agrego la class "hashtagLink" para referenciarla en agregarOnClickHashtags()
+    return `<a href="#" class="hashtagLink"> ${hashtag} </a>`;
+  }
+  /*
+   */
+
+  componentDidMount() {
+    //A: cuando componente montado trae textos y los setea al state
+    console.log("componente charla montado");
+    const obtenerTextosDeCharla = async () => {
+      const res = await leerTextos(this.props.idCharla);
+      const data = await res.json();
+      const textosConHashtagsLinks = this.hashtagsALinks(data.textos);
+      this.setState({ listaTextos: textosConHashtagsLinks });
+    };
+    obtenerTextosDeCharla();
   }
 
   render() {
     return html`
       <${Ons.Page} style=${{ display: "inline" }}>
         <${Ons.List} style=${{ marginTop: "3em" }}>
-          ${this.state.textos.map(
-            ( //TODO: gusanito cuando esta cargando las charlas
-              txt
-            ) => html`<${TarjetaTexto} textoCharla=${txt} > <//>`
-          )}
+          ${this.state.listaTextos.length === 0
+            ? "Cargando..."
+            : this.state.listaTextos.map(
+                (txt) => html`<${TarjetaTexto} textoCharla=${txt}> <//>`
+              )}
         <//>
       <//>
     `;
